fa0bc9a9521088a16191fb87ceeff5cefaed8d00
spacesone <149577258+spacesone@users.noreply.github.com>
2023-11-25 12:36:09 +0000

    CRUD operation done

--- api/v1/notesApi.js
+++ api/v1/notesApi.js
@@ -3,6 +3,8 @@ const router = require("express").Router();
 const edit = require("../../controllers/editNote").editNote;
 const getSingleNote = require("../../controllers/getSingleNote").getSingleNote;
 const search = require("../../controllers/search").search;
+const deleteNote = require("../../controllers/deleteNote").deleteNote;
+
 
 router.route('/add')
 .post(create)
@@ -15,5 +17,8 @@ router.route('/search/:queryString')
 
 router.route('/edit/:id')
 .put(edit)
+
+router.route('/delete/:id')
+.delete(deleteNote)
 
 module.exports = router;
---
+++ controllers/deleteNote.js
@@ -0,0 +1,48 @@
+const { Notes } = require("../models/Notes");
+
+const deleteNote = async (req, res) => {
+    const noteID = req.params.id;
+
+    if(!noteID){
+        return res.status(404).json({
+            error : true,
+            message : "id is required"
+        })
+    }
+
+        
+        
+        await Notes.destroy({
+            where : {
+                id : noteID
+            }
+        })
+        .then( result => {
+            if(result === 1) {
+                return res.status(200).json({
+                    ok : true,
+                    succcess : true,
+                    message : "deleted"
+                })
+            }
+            else {
+                return res.status(200).json({
+                    error : true,
+                    message : "can't delete"
+                })
+            }
+        })
+        .catch(error => {
+            return res.status(500).json({
+                error : true,
+                message : "error deleting record"
+            })
+        })
+
+
+
+ }
+
+ module.exports = {
+    deleteNote
+ }
